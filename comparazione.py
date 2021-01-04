041a9cf229ca461e88e6e7f29129f2bde294be9a
Davide Zordan <43811425+davidezordan25@users.noreply.github.com>
2021-01-04 16:34:13 +0100

    added 4-zone histograms + fixed weighted average

--- comparazione.py
+++ comparazione.py
@@ -18,21 +18,42 @@ def takeSecond(elem):
 
 def istogrammi(input):
     fsRead= cv2.FileStorage ("histograms.txt", cv2.FileStorage_READ )
-
+    
     #calcola istogramma immagine input
     image=cv2.imread(f"images/"+input)
     image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    hist = cv2.calcHist([image],[0, 1, 2], None,[8, 8, 8],[0, 256, 0, 256, 0, 256]) 
-    hist = cv2.normalize(hist, hist).flatten()
+    
+    hists={}
+    countc=0
+    countr=0
+    windowsize_r = math.ceil(image.shape[0]/2)
+    windowsize_c = math.ceil(image.shape[1]/2)
+ 
+    for r in range(0,image.shape[0] , windowsize_r):
+        for c in range(0,image.shape[1], windowsize_c):
+            window = image[r:r+windowsize_r,c:c+windowsize_c]
+            hist = cv2.calcHist([window],[0, 1, 2], None,[8, 8, 8],[0, 256, 0, 256, 0, 256]) 
+            hists[f'histoquery_{countr}_{countc}'] = cv2.normalize(hist, hist).flatten()
+            countc+= 1
+        countr+=1
+        countc=0
 
     results={}
-
+    
     image_names = os.listdir("gallery/")
     for filename in image_names:
         nomeFile = os.path.splitext(filename)[0]
-        hist_file=fsRead.getNode(f'histogram_{nomeFile}').mat()
-        compare=cv2.compareHist(hist,hist_file, 0)
-        results[nomeFile]=compare
+        compare=0 
+        for r in range(0,2):
+            for c in range(0,2):
+                hist_file=fsRead.getNode(f'histogram_{r}_{c}_{nomeFile}').mat()
+                compare+=cv2.compareHist(hists[f'histoquery_{r}_{c}'],hist_file, 0)
+                countc+= 1
+            countr+=1
+            countc=0
+        
+        media=compare/4
+        results[nomeFile]=media
 
 
     newdict2={k:v for k, v in sorted(results.items(),reverse=True,key=lambda key:key[1])}
@@ -58,7 +79,7 @@ def compara(input,num):
         for i in ids:
             if str(i) == first_n[x][0]:
                 
-                media = ((first_n[x][1]*100)*0.2 + (new[i])*0.8) / 1
+                media = ((first_n[x][1]*100)*0.25 + (new[i])*0.75) / 1
                 res.append((i,media))
     print(res)
     
