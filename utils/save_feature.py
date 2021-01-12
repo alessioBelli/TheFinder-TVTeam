179aa32ed853da5a190d8b5dbe62502f24f7cce6
Davide Zordan <43811425+davidezordan25@users.noreply.github.com>
2021-01-12 15:55:21 +0100

    Update save_feature.py

--- utils/save_feature.py
+++ utils/save_feature.py
@@ -8,7 +8,8 @@ import sys
 #Funzione per la creazione delle feature per ogni immagine del dataset
 def creazioneFeature():
     fe = FeatureExtractor()
-    #Controllo presenza cartella ./featurem, creazione se non ci dovesse essere
+   
+    #Controllo presenza cartella ./features, creazione se non ci dovesse essere
     if os.path.isdir("./features") == True:
         print("Directory già presente")
     else:
@@ -17,11 +18,12 @@ def creazioneFeature():
     image_names = os.listdir("gallery/")
 
     cont = 0
+    
     #Estrazione feature per ogni immagine e salvataggio nella cartella ./feature
     for filename in image_names:
         cont = cont+1
 
-        #Visualizzazione numero immagini eleborate nella console
+        #Visualizzazione numero immagini (a intervalli di 10) eleborate nella console
         if cont % 10 == 0:
             sys.stdout.write('\r' + "Le immagini elaborate sono attualmente : " + str(cont) + "/" + str(len(image_names)))
             sys.stdout.flush() 
@@ -30,11 +32,6 @@ def creazioneFeature():
             sys.stdout.flush() 
             
         nomeFile = os.path.splitext(filename)[0]
-        feature = fe.extract(img=Image.open(f"gallery/{nomeFile}.jpg"))
+        feature = fe.extract(img=Image.open(f"gallery/{nomeFile}.jpg"))             #richiamo il metodo extract della classe esterna featureExtractor
         feature_path = f"features/{nomeFile}.npy"
-        np.save(feature_path, feature)
-
-
-
-if __name__ == "__main__":
-    creazioneFeature()
+        np.save(feature_path, feature)                                              #salvo nel percorso specificato l'array Numpy (file .npy)
